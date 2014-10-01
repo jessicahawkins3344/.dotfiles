57e6704db7417306c64af15111343cc7d5ea992d
ChristopherA <ChristopherA@hackathon.net>
2014-10-01 12:33:44 -0700

    Added rvm to shell/bash_paths, but rvm install still gives woarning

--- install/installosxupdates.sh
+++ install/installosxupdates.sh
@@ -15,174 +15,186 @@ echo -e "\nChecking System -- \c"
 # If we on OSX, install OSX specific command line tools, brew, brew cask, etc.
 
 if [[ `uname` == 'Darwin' ]]; then
-  echo "we are installing on a Mac."
 
-  # Define some variables...
-  tmp_file=".softwareupdate.$$"
-  reboot=""
-  found_updates=""
+  # Get OSX Version
+  OSX_VERS=$(sw_vers -productVersion)
+  OSX_VERS_FIRST=$(sw_vers -productVersion | awk -F "." '{print $2}')
 
-  echo -e "\n  Checking Apple Software Update Server for available updates,\n  Please be patient. This process may take a while to complete... \c"
-  sudo /usr/sbin/softwareupdate -l &> $tmp_file
-  wait
+  echo "we are installing on a Mac under OSX $OSX_VERS."
 
-  echo -e "\n"
-  reboot=$(/usr/bin/grep "restart" $tmp_file | /usr/bin/wc -l | xargs )
-  echo "    $reboot updates require a reboot."
-  /usr/bin/grep "restart" $tmp_file
+  # on 10.9+, we can leverage Software Update to get the latest CLI tools
+  if [ "$OSX_VERS_FIRST" -ge 9 ];
+  then
 
-  echo ""
-  found_updates=$(/usr/bin/grep -v "restart" $tmp_file | grep "recommended" | /usr/bin/wc -l | xargs )
-  echo "    $found_updates updates do not require a reboot."
-  /usr/bin/grep -v "restart" $tmp_file | grep "recommended"
-  echo ""
+    # Define some variables...
+    tmp_file=".softwareupdate.$$"
+    reboot=""
+    found_updates=""
 
-  if [ $found_updates = "0" ]
-    then
-       echo "    No new recommended updates found."
-    else
-      if [ $reboot = "0" ]
+    echo -e "\n  Checking Apple Software Update Server for available updates,\n  Please be patient. This process may take a while to complete... \c"
+    sudo /usr/sbin/softwareupdate -l &> $tmp_file
+    wait
+
+    echo -e "\n"
+    reboot=$(/usr/bin/grep "restart" $tmp_file | /usr/bin/wc -l | xargs )
+    echo "    $reboot updates require a reboot."
+    /usr/bin/grep "restart" $tmp_file
+
+    echo ""
+    found_updates=$(/usr/bin/grep -v "restart" $tmp_file | grep "recommended" | /usr/bin/wc -l | xargs )
+    echo "    $found_updates updates do not require a reboot."
+    /usr/bin/grep -v "restart" $tmp_file | grep "recommended"
+    echo ""
+
+    if [ $found_updates = "0" ]
       then
-        echo "    Updates found, but no reboot required. Installing now."
-        echo "    Please be patient. This process may take a while to complete."
-        sudo /usr/sbin/softwareupdate -ia
-        wait
-        echo -e "\n  Finished with all Apple Software Update installations."
+         echo "    No new recommended updates found."
       else
-        echo "    Updates found, reboot required. Installing now."
-        echo "    Please be patient. This process may take a while to complete."
-        echo -e "    Once complete, this machine will automatically restart.\n"
-        sudo /usr/sbin/softwareupdate -ia
-        wait
-        echo -e "    Finished with all Apple Software Update installations."
+        if [ $reboot = "0" ]
+        then
+          echo "    Updates found, but no reboot required. Installing now."
+          echo "    Please be patient. This process may take a while to complete."
+          sudo /usr/sbin/softwareupdate -ia
+          wait
+          echo -e "\n  Finished with all Apple Software Update installations."
+        else
+          echo "    Updates found, reboot required. Installing now."
+          echo "    Please be patient. This process may take a while to complete."
+          echo -e "    Once complete, this machine will automatically restart.\n"
+          sudo /usr/sbin/softwareupdate -ia
+          wait
+          echo -e "    Finished with all Apple Software Update installations."
+        fi
       fi
+
+    # cleaning up temp files before possible reboot
+    /bin/rm -rf $tmp_file
+
+    if [ $reboot != "0" ]
+    then
+      echo -e "\n  Apple Software Updates requiring restart have been installed."
+      echo -e "  Please run this script again after restart.\n"
+      read -p "Press any key to restart..." </dev/tty
+      wait
+      echo -e "\nRestarting..."
+      sudo /sbin/shutdown -r now
     fi
 
-  # cleaning up temp files before possible reboot
-  /bin/rm -rf $tmp_file
+    echo -e "\n  Checking to see if Apple Command Line Tools are installed."
+    xcode-select -p &>/dev/null
+    if [[ $? -ne 0 ]]
+    then
+      echo "    Apple Command Line Utilities not installed. Installing..."
+      echo "    Please be patient. This process may take a while to complete."
 
-  if [ $reboot != "0" ]
-  then
-    echo -e "\n  Apple Software Updates requiring restart have been installed."
-    echo -e "  Please run this script again after restart.\n"
-    read -p "Press any key to restart..." </dev/tty
-    wait
-    echo -e "\nRestarting..."
-    sudo /sbin/shutdown -r now
-  fi
+      # Tell software update to also install OXS Command Line Tools without prompt
+      ## As per https://sector7g.be/posts/installing-xcode-command-line-tools-through-terminal-without-any-user-interaction
 
-  echo -e "\n  Checking to see if Apple Command Line Tools are installed."
-  xcode-select -p &>/dev/null
-  if [[ $? -ne 0 ]]
-  then
-    echo "    Apple Command Line Utilities not installed. Installing..."
-    echo "    Please be patient. This process may take a while to complete."
+      touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
-    # Tell software update to also install OXS Command Line Tools without prompt
-    ## As per https://sector7g.be/posts/installing-xcode-command-line-tools-through-terminal-without-any-user-interaction
+      sudo /usr/sbin/softwareupdate -ia
+      wait
 
-    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+      /bin/rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+      echo -e "\n    Finished installing Apple Command Line Tools."
+    else
+      echo -e "\n    Apple Command Line Tools already installed."
+    fi
 
-    sudo /usr/sbin/softwareupdate -ia
-    wait
+    # Update Ruby
 
-    /bin/rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-    echo -e "\n    Finished installing Apple Command Line Tools."
-  else
-    echo -e "\n    Apple Command Line Tools already installed."
-  fi
+    echo -e "\n  Updating Ruby,,."
+    echo "  Please be patient. This process may take a while to complete."
 
-  # Update Ruby
+    # disable documentation installation to speed up gem update
 
-  echo -e "\n  Updating Ruby,,."
-  echo "  Please be patient. This process may take a while to complete."
+    # Don't install documentation
+    echo "gem: --no-document" >> ~/.gemrc
 
-  # disable documentation installation to speed up gem update
+    sudo gem update --system
+    curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enable
+    sudo gem update --system
 
-  # Don't install documentation
-  echo "gem: --no-document" >> ~/.gemrc
+    echo -e "\n  Ruby updated."
 
-  sudo gem update --system
-  curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enable
-  sudo gem update --system
+    source ~/.rvm/scripts/rvm
 
-  echo -e "\n  Ruby updated."
-
-  source ~/.rvm/scripts/rvm
-
-  echo -e "\n  Ruby is installed and updated."
+    echo -e "\n  Ruby is installed and updated."
 
 
-  # Install Homebrew http://brew.sh or search for brew formulae at http://braumeister.org
+    # Install Homebrew http://brew.sh or search for brew formulae at http://braumeister.org
 
-  echo -e "\n  Checking to see if Homebrew is installed."
+    echo -e "\n  Checking to see if Homebrew is installed."
 
-  if ! which brew &> /dev/null
-  then
-    echo "    Homebrew not installed. Installing..."
-    echo "    Please be patient. This process may take a while to complete."
+    if ! which brew &> /dev/null
+    then
+      echo "    Homebrew not installed. Installing..."
+      echo "    Please be patient. This process may take a while to complete."
 
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-    echo -e "\n    Finished installing Homebrew."
+      echo -e "\n    Finished installing Homebrew."
 
-  else
-      echo -e "\n    Homebrew already installed."
-  fi
+    else
+        echo -e "\n    Homebrew already installed."
+    fi
 
-  echo "    Checking Homebrew configuration..."
+    echo "    Checking Homebrew configuration..."
 
-  brew doctor
-  brew update
-  brew upgrade
-  brew linkapps
-  brew cleanup
-  brew prune
+    brew doctor
+    brew update
+    brew upgrade
+    brew linkapps
+    brew cleanup
+    brew prune
 
-  # Install Brew Cask: http://caskroom.io
+    # Install Brew Cask: http://caskroom.io
 
-  echo -e "\n  Checking to see if Brew Cask is installed."
+    echo -e "\n  Checking to see if Brew Cask is installed."
 
-  if ! brew cask &> /dev/null
-  then
-    echo "    Cask not installed. Installing..."
-    echo "    Please be patient. This process may take a while to complete."
+    if ! brew cask &> /dev/null
+    then
+      echo "    Cask not installed. Installing..."
+      echo "    Please be patient. This process may take a while to complete."
 
-    brew install caskroom/cask/brew-cask
+      brew install caskroom/cask/brew-cask
 
-    echo -e "\n    Finished installing Cask."
+      echo -e "\n    Finished installing Cask."
 
-  else
-      echo -e "\n    Cask already installed, updating..."
-      brew upgrade brew-cask &> /dev/null
-  fi
+    else
+        echo -e "\n    Cask already installed, updating..."
+        brew upgrade brew-cask &> /dev/null
+    fi
 
-  echo "    Checking Brew Cask configuration..."
+    echo "    Checking Brew Cask configuration..."
 
-  brew cleanup
-  brew cask cleanup
+    brew cleanup
+    brew cask cleanup
 
-  echo "    Cask installed and updated."
+    echo "    Cask installed and updated."
 
-  # Install essential Brew and Casks here
+    # Install essential Brew and Casks here
 
-  brew install git # http://git-scm.com
-  brew install hub # https://hub.github.com
-  brew install get-extras # https://github.com/visionmedia/git-extras
-  brew install bash-completion # http://bash-completion.alioth.debian.org
+    brew install git # http://git-scm.com
+    brew install hub # https://hub.github.com
+    brew install git-extras # https://github.com/visionmedia/git-extras
+    brew install bash-completion # http://bash-completion.alioth.debian.org
 
-  # Cleaning up Brew and Cask caches
+    # Cleaning up Brew and Cask caches
 
-  echo "Cleaning up Brew and brew Cask caches"
-  brew linkapps
-  brew cleanup
-  brew prune
-  brew cask cleanup
+    echo "Cleaning up Brew and brew Cask caches"
+    brew linkapps
+    brew cleanup
+    brew prune
+    brew cask cleanup
 
-  # The whathis database, used by whatis and apropos, is only generated weekly, so run it after adding commands.
+    # The whathis database, used by whatis and apropos, is only generated weekly, so run it after adding commands.
 
-  sudo periodic weekly
+    sudo periodic weekly
 
+ else
+   echo "This script only supports OSX 10.9 Mavericks or better! Exiting..."
+ fi
 
 else
   echo "We are not running on a Mac! Install scripts for non-Macs are a work-in-progress."
