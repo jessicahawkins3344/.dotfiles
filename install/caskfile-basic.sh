5850c5ca8b74a4d2a14ffd69b93fc5328a6d500d
ChristopherA <ChristopherA@hackathon.net>
2014-10-08 10:45:57 -0700

    Added Dash http://kapeli.com/dash to casks and mackup

--- install/caskfile-basic.sh
+++ install/caskfile-basic.sh
@@ -8,12 +8,15 @@
 # Note that `mackup` is installed by brewfile-basic, so dont' forget to update
 # the ~/dotfiles/mackup.cfg file if you want settings backed up or restored.
 
-# Essentials
+# Essential Utilities
 brew cask install dropbox
-brew cask install atom
 brew cask install mailplane
 brew cask install sizeup
+
+# Dev Tools
+brew cask install atom
 brew cask install google-chrome
+brew cask install dash # http://kapeli.com/dash
 
 # Less essential
 brew cask install transmission
