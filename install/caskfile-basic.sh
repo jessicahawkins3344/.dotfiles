cd38fa7c4f40f0828cbf0d1a2c80ee0954ea0bfd
ChristopherA <ChristopherA@hackathon.net>
2015-03-18 20:35:18 -0700

    Removed Android Dev tools

--- install/caskfile-basic.sh
+++ install/caskfile-basic.sh
@@ -5,9 +5,6 @@
 #
 # also sourced by `allosxupdates.sh`
 
-# Note that `mackup` is installed by brewfile-basic, so dont' forget to update
-# the ~/dotfiles/mackup.cfg file if you want settings backed up or restored.
-
 # Essential Utilities
 brew cask install dropbox
 brew cask install mailplane
@@ -18,13 +15,7 @@ brew cask install atom
 brew cask install google-chrome
 brew cask install dash # http://kapeli.com/dash
 brew cask install github #https://mac.github.com
-
-# Android Development Tools
 brew cask install java # http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
-brew cask install android-studio-bundle http://developer.android.com/sdk/installing/studio.html
-#! add adb to path?
-brew cask install virtualbox # https://www.virtualbox.org
-brew cask install genymotion # http://www.genymotion.com
 
 # Less essential
 brew cask install transmission
